ea5680de5c4343f26593a59303e05a23202ac631
Linxusthuis <140453732+Linxusthuis@users.noreply.github.com>
2024-09-27 09:37:15 -0300

    sdfdg

--- main.js
+++ main.js
@@ -13,8 +13,10 @@ function quadrado(lado){
     return lado**2;
 }
 console.log(quadrado(6));
-let media = parseInt(prompt('quais 3 numeros para achar a media?'))
+let media = parseInt(prompt('qual o 1º dos 3 numeros para achar a media?'));
+let media1 = parseInt(prompt('qual o 2º dos 3 numeros para achar a media?'));
+let media2 = parseInt(prompt('qual o 3º dos 3 numeros para achar a media?'));
 let medias = function(){
-    return (media)/3;
+    return (media+media1+media2)/3;
 }
-console.log(medias(media));
+console.log(medias());
