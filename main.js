3e7c4a462fd4ce180765053c7da46eb48d4841c8
Linxusthuis <140453732+Linxusthuis@users.noreply.github.com>
2024-09-27 08:58:19 -0300

    sd

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
+document.querySelector('butt')
+butt.onclick
+
 function soma(num1,num2){
     return num1 + num2;
 }
 
 console.log(soma(5,3));
 
-function quadrado(num1,num2){
-    return num1 * num2;
+function quadrado(lado)){
+    return lado**2;
 }
+console.log(quadrado(6));
+
+let media = function(num1,num2,num3){
+    return (num1+num2+num3)/3
+}
+console.log(media(1,2,3))
