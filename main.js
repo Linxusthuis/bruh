f09a8ccce62f295a36509c289940f705b98ebc05
Linxusthuis <140453732+Linxusthuis@users.noreply.github.com>
2024-10-04 09:24:10 -0300

    argaerhgarh

--- main.js
+++ main.js
@@ -21,15 +21,22 @@
 // }
 // console.log(medias(media,media1,media2));
 
-let estados = ['AC','AL','AP','AM','BA','CE','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO','DF'];
+// let estados = ['AC','AL','AP','AM','BA','CE','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO','DF'];
 
 // let i = 0;
 // while (i<estados.length){
 //     console.log(estados[i]);
 //     i++;
 // };
-for (let f = 0; f < estados.length; f++) {
-    console.log(estados[f]);
-    console.log(f)
+// for (let f = 0; f < estados.length; f++) {
+//     console.log(estados[f]);
+//     console.log(f)
     
+// }
+let borg = parseInt(prompt('Borg?'))
+if (borg>17){
+    console.log('zirb')
+}
+else{
+    console.log('zirg')
 }
