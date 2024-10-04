76eabb9bfb8f1cee99d9ddda7f135e05f5283504
Linxusthuis <140453732+Linxusthuis@users.noreply.github.com>
2024-10-04 08:28:43 -0300

    svdlvsdlbisd

--- main.js
+++ main.js
@@ -1,22 +1,24 @@
- var butt = document.getElementsByName('butt')
-function gay(){
-    alert('caguei');
- }
+//  var butt = document.getElementsByName('butt')
+// function gay(){
+//     alert('caguei');
+//  }
 
-function soma(num1,num2){
-    return num1 + num2;
-}
+// function soma(num1,num2){
+//     return num1 + num2;
+// }
 
-console.log(soma(5,3));
+// console.log(soma(5,3));
 
-function quadrado(lado){
-    return lado**2;
-}
-console.log(quadrado(6));
-let media = parseInt(prompt('qual o 1º dos 3 numeros para achar a media?'));
-let media1 = parseInt(prompt('qual o 2º dos 3 numeros para achar a media?'));
-let media2 = parseInt(prompt('qual o 3º dos 3 numeros para achar a media?'));
-let medias = function(num1,num2,num3){
-    return (num1+num2+num3)/3;
-}
-console.log(medias(media,media1,media2));
+// function quadrado(lado){
+//     return lado**2;
+// }
+// console.log(quadrado(6));
+// let media = parseInt(prompt('qual o 1º dos 3 numeros para achar a media?'));
+// let media1 = parseInt(prompt('qual o 2º dos 3 numeros para achar a media?'));
+// let media2 = parseInt(prompt('qual o 3º dos 3 numeros para achar a media?'));
+// let medias = function(num1,num2,num3){
+//     return (num1+num2+num3)/3;
+// }
+// console.log(medias(media,media1,media2));
+
+let estados = ['AC','AL','AP','AM','BA','CE','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO','DF'];
