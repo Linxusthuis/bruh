80470d754cae61158a669266bae28ad8fd051cab
Linxusthuis <140453732+Linxusthuis@users.noreply.github.com>
2024-10-04 08:56:01 -0300

    874

--- main.js
+++ main.js
@@ -24,7 +24,7 @@
 let estados = ['AC','AL','AP','AM','BA','CE','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO','DF'];
 
 let i = 0;
-while (i<28){
+while (i<27){
     console.log(estados[i]);
     i = i+1;
 };
